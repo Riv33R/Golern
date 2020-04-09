f9fc53343b4ef9b200027ca779a580800bb9b64f
Riv33R <63343338+Riv33R@users.noreply.github.com>
2020-04-09 21:32:41 +0300

    Add files via upload

---
+++ Tasc №1.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var (
+	idNumber int
+	rstring  string
+	webPrint string
+)
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	web()
+
+}
+
+func random()  {
+	idNumber = rand.Intn(1000)
+
+}
+
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63() % int64(len(letterBytes))]
+	}
+	return string(b)
+}
+
+
+
+func jsonobject()  {
+
+	x := map[string]interface{}{"IDNumber": idNumber, "Feels": rstring}
+	b, err := json.MarshalIndent(x, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println("RANDOM GENERATOR")
+	//fmt.Println(string(b))
+	webPrint = string(b)
+}
+
+func web()  {
+
+	for  {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+			rstring = RandStringBytes(10)
+			fmt.Fprint(w, webPrint)
+			random()
+			jsonobject()
+
+		})
+		http.ListenAndServe(":80", nil)
+	}
+
+}
